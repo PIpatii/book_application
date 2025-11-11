292701c513a7ec7d1ae2695f8607567896cc8dfa
Petro <petroipatiy@gmail.com>
2025-11-11 10:49:51 +0000

    second update

--- src/main/java/mate/academy/application/controller/AuthController.java
+++ src/main/java/mate/academy/application/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "User", description = "Users")
+@Tag(name = "User", description = "registration of users")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
